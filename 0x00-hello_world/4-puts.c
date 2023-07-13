92d31edefe636e216d6ce4c7187670c0927adc52
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-13 15:48:35 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This is a program that prints using puts command
+ * Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
