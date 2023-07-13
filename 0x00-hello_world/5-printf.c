90e161967fdad6ee205794ab4749d9bd599be50b
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-13 16:01:13 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ * Description: this is a program that print using prinf command of C
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
