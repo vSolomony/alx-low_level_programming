58bf366c9b950cc3b68cb3aa96de418f546e9e2f
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-14 16:50:21 -0700

    1 #include <stdio.h>

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry level
+* Description: 
+* return: Always 0 (sucess)
+*/
+int main(void)
+{	
+	int a = 0;
+
+	while (a <= 9);
+	{
+		putchar(a + '0');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
