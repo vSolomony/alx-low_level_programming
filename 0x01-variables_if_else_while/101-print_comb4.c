3b85f49b689c56457d76f7c42cf67f6f9d4f9f52
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-14 18:54:31 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,27 +14,31 @@ int main(void)
 		digit2 = 0;
 		while (digit3 <= 9)
 		{
-			if (digit1 != digit2 &&
-			    digit1 < digit2 &&
-			    digit2 != digit3 &&
-			    digit2 < digit3)
+			digit3 = 0;
+			while (digit3 <= 9)
 			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
-				putchar(digit3 + 48);
-
-				if (digit1 + digit2 + digit3 != 24)
+				if (digit1 != digit2 &&
+				    digit1 < digit2 &&
+				    digit2 != digit3 &&
+				    digit2 < digit3)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
+
+					if (digit1 + digit2 + digit3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+				digit3++;
 			}
-			digit3++;
+			digit2++;
 		}
-		digit2++;
+		digit1++;
 	}
-	digit1++;
 	putchar('\n');
 
-return (0);
+	return (0);
 }
