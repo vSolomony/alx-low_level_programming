7fd46d626e68c213252aaefb5f55f85037d2b53f
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-18 14:51:13 -0700

    4.isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_isalpha - checks for alphabetic character.
+ * @c: checks for alphabetic character.
+ *Return: Alawys 0 (true)
+*/
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
