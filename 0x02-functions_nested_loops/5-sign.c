01605b50dd27c4313629c3510a1cec97556f9f0b
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-18 15:22:11 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,12 +7,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
 		_putchar('+');
+	}
 	else if (n == 0)
+	{
 		return (0);
 		_putchar('0');
-	else (n < 0)
+	}
+	else
+	{
 		return (-1);
-		_putchar('-');
+		_putchar(-);
+	}
 }
