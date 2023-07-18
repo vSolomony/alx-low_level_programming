e759570e07b03d0050b7852639a6691af7f4187e
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-18 14:27:51 -0700

    10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,15 @@
-#include "main.h"
+#include"main.h"
 /**
  *print_alphabet_x10 - prints 10 times the alphabet
  *Return: Alawys 0 (Sucess)
 */
 void print_alphabet_x10(void)
 {
-	int row;
-	int bo;
+	int row, ch;
 
 	for (row = 1; row <= 10; row++)
 	{
-	for (bo = 'a'; bo <= 'z'; bo++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(bo);
 		_putchar('\n');
 	}
