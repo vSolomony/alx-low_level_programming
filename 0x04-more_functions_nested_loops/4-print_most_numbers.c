ceffc620e3844cca08efadaa1631c2f6ea1d76a1
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-20 13:27:35 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,18 @@
 /**
  *print_most_numbers - print most numbers
  *dont print 2 and 4
+ *Return: Always 0 (Sucess)
 */
 void print_most_numbers(void)
 {
 	int no;
 
-	for (no = 0; no <= 9; no++)
+	for (no = 48; no <= 57; no++)
 	{
-		if (no == 2 && no == 4)
+		if (no == 50 && no == 52)
 			continue;
 		else
-			_putchar(no + 48);
+			_putchar(no);
 	}
 	_putchar('\n');
 }
