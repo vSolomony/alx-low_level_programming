7bfd8df563bbf8dd8e83f14ddd677f9f045e7a32
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-20 13:04:45 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers - from 0-9 prints
+ *followed by new line
+*/
+void print_numbers(void)
+{
+	int v;
+
+	for (v = 48; v <= 57; v++)
+		_putchar(v);
+	_putchar('\n');
+}
