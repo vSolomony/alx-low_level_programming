62f8adad77ba420ace7e681f85877335a2a3a3e3
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-24 19:23:12 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+		++count;
+
+	return (count);
+}
