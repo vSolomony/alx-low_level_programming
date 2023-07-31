e778ea5af34c28e97349ab1271d8135c221bf98d
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-31 07:28:38 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,13 +3,13 @@
  * _strchr - function that locates a character in a string
  * @s: array
  * @c: char
- * Return: dest
+ * Return: NULL or S
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
-	for (i = 0; s >= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 	if (s[i] == c)
 	return (s + i);
