fea83724977182e6547c92ae4fc57f98fd3d8267
vSolomony <mohamed.a.kassem95@gmail.com>
2023-07-31 08:18:53 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: array
+ * @needle: array
+ * Return: sucess
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+
+		if (*j == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+
