b531b96a6cc2ac3401e489e262e98a977bf50383
vSolomony <mohamed.a.kassem95@gmail.com>
2023-08-04 16:14:37 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry level
+ * @argc: argument
+ * @argv: argument
+ * Return: Always 0 (Sucess)
+*/
+int main(int argc, char* argv[])
+{
+	(void)argc;
+	printf("%s/n", argv[0]);
+
+	return (0);
+}
