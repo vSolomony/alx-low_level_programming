41cd3ef1310efa68522b254452d086ba2ee6326c
vSolomony <mohamed.a.kassem95@gmail.com>
2023-08-14 13:18:18 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,17 @@
-struct dog{
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - a dog struct
+ * @name: is name of dog
+ * @age: is age of dog
+ * @owner: is owner of dog
+ * Description: a dog struct that has different features
+*/
+struct dog
+{
 char *name;
 float age;
 char *owner;
 };
+
+#endif
