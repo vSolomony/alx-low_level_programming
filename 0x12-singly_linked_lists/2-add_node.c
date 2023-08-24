193674f66a61f1310d655fcd2d4bcea05cb19caa
vSolomony <mohamed.a.kassem95@gmail.com>
2023-08-24 13:27:30 -0700

    add node at the begg of list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_node - new node at begging of list
+ * @head: double pointer
+ * @str: string
+ * Return: address of the new elements
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = malloc(sizeof(list_t));
+
+	if (head == NULL || ptr == NULL)
+		return (NULL);
+
+	ptr->str = strdup(str);
+	ptr->len = strlen(ptr->str);
+	ptr->next = NULL;
+	ptr->next = *head;
+	*head = ptr;
+
+	return (*head);
+}
