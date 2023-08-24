c186aeda798c85ecdfecdf62675d6131343d8d18
vSolomony <mohamed.a.kassem95@gmail.com>
2023-08-24 14:19:08 -0700

    4th task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,8 +1,7 @@
 #include "lists.h"
-
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ * free_list - free the link list.
+ * @head : head that will be freed
  */
 void free_list(list_t *head)
 {
