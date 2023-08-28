4b3ad42f5b99b90b6fee2f14952db44cdb03b086
vSolomony <mohamed.a.kassem95@gmail.com>
2023-08-28 08:18:17 -0700

    1st task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+include "lists.h"
+/**
+ * print_listint - printing and returning the list values
+ * @h: the header of the list
+ * Return: the number of values
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	if (h == NULL)
+		return (-1);
+	
+	printf("%d\n", h->n);
+	printf("%d\n", *h->next);
+   
+	while(h != NULL)
+	{   
+		h =h->next;
+		count++;
+	}
+	return (count);
+}
