5d9031b7745157c4204def7d42daee3a12007631
vSolomony <mohamed.a.kassem95@gmail.com>
2023-08-29 08:57:37 -0700

    8th taskl

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - values of a specific node
+ * @head: address of the head
+ * @index: index of the node
+ * Return: the address of the selected node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int i = 0;
+
+		if (head == NULL)
+			return (NULL);
+
+		while (i < index)
+		{
+			head = head->next;
+			i++;
+		}
+		return (head);
+}
