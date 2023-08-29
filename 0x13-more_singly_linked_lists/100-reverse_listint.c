0d882fd5ffa5c1930a936c086c37ce01fcc5c1ad
vSolomony <mohamed.a.kassem95@gmail.com>
2023-08-29 13:02:41 -0700

    1st advanced task

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverse node list
+ * @head: double pointer that hold addrss of head node
+ * Return: the reverse list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev_node, *curr_node, *next_node;
+
+	prev_node = 0;
+	curr_node = next_node = head;
+
+	while (next_node != NULL)
+	{
+		next_node = curr_node->next;
+		curr_node->next = prev_node;
+		prev_node = curr_node;
+		curr_node = prev_node;
+	}
+
+	*head = prev_node;
+
+	return (*head);
+}
+
