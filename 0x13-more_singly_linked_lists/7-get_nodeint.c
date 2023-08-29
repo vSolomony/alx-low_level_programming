caf5239b99b6dd9023d2da2f9fab79417780aee1
vSolomony <mohamed.a.kassem95@gmail.com>
2023-08-29 09:08:07 -0700

    8th task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		if (head == NULL)
 			return (NULL);
 
-		while (i < index)
+		while (i < index && head != NULL)
 		{
 			head = head->next;
 			i++;
