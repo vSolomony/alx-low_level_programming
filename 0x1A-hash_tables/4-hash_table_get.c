6df2c0239a35abb65c97d7c46c9d61236f33a839
vSolomony <mohamed.a.kassem95@gmail.com>
2023-11-02 14:31:54 -0700

    123

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-		return NULL;
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
@@ -22,10 +22,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
-			return node->value;
+			return (node->value);
 		node = node->next;
 	}
 
-	return NULL;
+	return (NULL);
 }
 
